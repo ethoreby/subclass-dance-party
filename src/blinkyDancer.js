694e1b30c5787caeff7f3c6918663d2c18e4391f
Hack Reactor Students <ethoreby@gmail.com>
2014-03-03 16:23:38 -0800

    HTML and specs accommodate pseudoclassical instantiation

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,12 +1,11 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps){
-
-  this.oldStep = Dancer.prototype.step;
   Dancer.call(this, top, left, timeBetweenSteps);
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 BlinkyDancer.prototype.step = function(){
-  this.oldStep();
+  Dancer.prototype.step.call(this);
+  //Dancer.prototype.step();
   this.$node.toggle();
 };
